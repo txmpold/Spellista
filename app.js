b42d637f9fe0355d512eb012eee9b563cffac6fc
txmpold <love.tampold@hotmail.com>
2026-02-04 18:02:13 +0100

    added imports for controller, model and view

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+import { Controller } from "./controller.js";
+import { Model } from "./model.js";
+import { View } from "./view.js";
+
 document.addEventListener("DOMContentLoaded", initApp);
 
 const initApp = () => {};
