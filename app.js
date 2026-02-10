88cbd52654fe4a8a534b3350d6ccb0e54c5103a6
txmpold <love.tampold@hotmail.com>
2026-02-10 17:33:02 +0100

    removed js code

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-import { Controller } from "./controller.js";
-import { Model } from "./model.js";
-import { View } from "./view.js";
-
 document.addEventListener("DOMContentLoaded", initApp);
 
 const initApp = () => {};
